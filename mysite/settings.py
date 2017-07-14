6612d3a4862c6957cee49b4b2860b325a6e40bcb
archanaraghu512 <archana.iyer1991@gmail.com>
2017-07-14 15:22:42 -0500

    Procfile

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,8 +138,11 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
+
 DATABASES['default'].update(db_from_env)
